552b4607a0435ae069490bf39f8a0c0cfe5d637f
CmdBianchi <marcusbianchi92@hotmail.com>
2021-08-16 16:32:45 -0300

    Adicionar arquivos de projeto.

---
+++ RegEx/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+namespace RegEx {
+    class Program {
+        static void Main(string[] args) {
+
+            var wsText = "ADTK BY 19AUG21 1434GRU 19AUG21 1434SAO PER 4PLUSTA";
+            var match = Regex.Match(wsText, @"(?<date>[0-9]{1,2}[A-Z]{3}[0-9]{2}).*(?<hours>[0-9]{4})");
+            var dateText = match.Groups["date"].Value;
+            var hoursText = match.Groups["hours"].Value;
+            Console.WriteLine(wsText);
+            var date = DateTime.ParseExact($"{dateText} {hoursText}", "ddMMMyy HHmm", CultureInfo.InvariantCulture);
+            Console.WriteLine(date.ToString());
+
+        }
+    }
+}
